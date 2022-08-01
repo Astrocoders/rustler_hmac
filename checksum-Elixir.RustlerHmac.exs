f4a73309a005dce619e98cebfb915b5bd89a7942
George Lima <georgelima11@hotmail.com>
2022-08-01 09:45:51 -0300

    chore: push checksums

---
+++ checksum-Elixir.RustlerHmac.exs
@@ -0,0 +1,10 @@
+%{
+  "hmac-v0.0.1-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:9b889be47a51c69e6ffabebe2e34eeecaf9a4ee1080829899f474bbf6dcc4984",
+  "hmac-v0.0.1-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:a972b38303732047de5ade7125553afb656234c8217bf125658fb9e0a05a30e3",
+  "libhmac-v0.0.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:f99757c78544215ebb9602f763ac1e2058a49716718b86480ee7dd3f9846e62d",
+  "libhmac-v0.0.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:1ba335be25d938fbdfd0c84d863bda7677f535fc59c5e3bd8ac4a08826a2cc18",
+  "libhmac-v0.0.1-nif-2.16-arm-unknown-linux-gnueabihf.so.tar.gz" => "sha256:2797d5eba992dfb1ab8c3d3aee0647fb75c5d89b85bb83b040754cacca0dfd18",
+  "libhmac-v0.0.1-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:f93921b69d5f54ab37355a2739d1738d289b04e4a0082dfbd5b2a78abdb64e3f",
+  "libhmac-v0.0.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c3df90ac95b48e7a94ce04fec6d669989718d884be9a330f9371a75c74a8e509",
+  "libhmac-v0.0.1-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:e40bd047b54af33b8a9f8331298367797a448c31d0f701dc67eb0ea044af6e84",
+}
